ae802a0a7f3ceb0453c5ef74873103bb12322f90
Kaique Cunha Mori <cunhakaique004@gmail.com>
2026-08-20 16:39:19 -0300

    adicionando os botoes

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,6 @@ import interfaceDoComando.JanelaPrincipal;
 public class Main {
     public static void main(String[] args) {
         JanelaPrincipal j = new JanelaPrincipal();
-        j.jenelaPrincipal();
+
     }
 }
--- src/interfaceDoComando/JanelaPrincipal.java
+++ src/interfaceDoComando/JanelaPrincipal.java
@@ -4,21 +4,65 @@ import javax.swing.*;
 
 public class JanelaPrincipal {
 
-    public JFrame jenelaPrincipal () {
-        JFrame janela = new JFrame();
-        janela.setSize(1500,800);
+    private JFrame janela;
+    private JPanel painel;
+
+    public JanelaPrincipal() {
+
+        janela = new JFrame("Interface do Comando");
+        janela.setSize(1500, 800);
         janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         janela.setLocationRelativeTo(null);
-        janela.setVisible(true);
+
+        painel = new JPanel();
+        painel.setLayout(null);
+
+        janela.add(painel);
 
         adicionarBotoes();
-        return janela;
+
+        janela.setVisible(true);
     }
 
-    public void adicionarBotoes(){
-        Botoes b = new Botoes();
-        jenelaPrincipal().add(b.novo())
-                .setBounds(10, 80, 300, 125);
+    public void adicionarBotoes() {
 
+        Botoes b = new Botoes();
+
+        JButton novo = b.novo();
+        novo.setBounds(10, 10, 150, 50);
+
+        JButton abrir = b.abrir();
+        abrir.setBounds(10, 70, 150, 50);
+
+        JButton salvar = b.salvar();
+        salvar.setBounds(10, 130, 150, 50);
+
+        JButton copiar = b.copiar();
+        copiar.setBounds(10, 190, 150, 50);
+
+        JButton colar = b.colar();
+        colar.setBounds(10, 250, 150, 50);
+
+        JButton recortar = b.recortar();
+        recortar.setBounds(10, 310, 150, 50);
+
+        JButton compilar = b.compilar();
+        compilar.setBounds(10, 370, 150, 50);
+
+        JButton equipe = b.equipe();
+        equipe.setBounds(10, 430, 150, 50);
+
+        painel.add(novo);
+        painel.add(abrir);
+        painel.add(salvar);
+        painel.add(copiar);
+        painel.add(colar);
+        painel.add(recortar);
+        painel.add(compilar);
+        painel.add(equipe);
+    }
+
+    public static void main(String[] args) {
+        new JanelaPrincipal();
     }
 }
